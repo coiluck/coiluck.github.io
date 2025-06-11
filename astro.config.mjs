de5db6d7d0dfc0f43d4d558b3e8dc55eb058d1b3
coiluck <wvy.wingle@gmail.com>
2025-06-12 01:08:52 +0900

    Replace existing site with Astro
    Now this site runs entirely on Astro!

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// Markdownプラグインをインポート
+import remarkGfm from 'remark-gfm';
+import remarkFootnotes from 'remark-footnotes';
+import remarkAttributes from 'remark-attributes'; 
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark'
+    },
+    remarkPlugins: [
+      remarkGfm,
+      remarkFootnotes,
+      remarkAttributes 
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeAutolinkHeadings
+    ],
+    allowHTML: true
+  },
+  build: {
+    format: 'directory'
+  },
+  site: 'https://coiluck.moe'
+});
---
+++ src/data/link-data.js
@@ -0,0 +1,78 @@
+export const createData = {
+  title: "制作物",
+  description: "つくったものなど",
+  links: [
+    {
+      url: "https://coiluck.github.io/nongenerate-generater/",
+      title: "絶対に決める",
+      description: "何か決めたいとき用"
+    },
+    {
+      url: "https://coiluck.github.io/anya/",
+      title: "アーニャのお仕事体験記",
+      description: "ギャグノベルゲーム"
+    },
+    {
+      url: "https://coiluck.github.io/truth-edokko/",
+      title: "真の江戸っ子になれ！",
+      description: "お金を使い切るゲーム"
+    },
+    {
+      url: "https://coiluck.github.io/CountryChain/",
+      title: "国境しりとり",
+      description: "接している国を交互に言い合うゲーム"
+    },
+    {
+      url: "https://coiluck.github.io/SinkendenSonne/",
+      title: "陽の沈む場所",
+      description: "月面を開発するノベルゲーム"
+    },
+    {
+      url: "https://coiluck.github.io/history-game/",
+      title: "歴史の扉",
+      description: "人物の年代を比べるゲーム"
+    },
+    {
+      url: "https://coiluck.github.io/CardCreater/",
+      title: "カード作成ツール",
+      description: "オリジナルのカードを作成するツール"
+    }
+  ]
+};
+
+export const linksData = {
+  title: "リンク集",
+  description: "僕が使っているアカウントなど",
+  links: [
+    {
+      url: "https://github.com/coiluck",
+      title: "GitHub",
+      description: "Pages用のリポジトリしかない"
+    },
+    {
+      url: "https://www.chichi-pui.com/users/coiluck/",
+      title: "chichi-pui",
+      description: "AIイラスト専用の投稿サイト"
+    },
+    {
+      url: "https://www.pixiv.net/users/63923759",
+      title: "pixiv",
+      description: "更新しない"
+    },
+    {
+      url: "https://truthsocial.com/@coiluck",
+      title: "Truth Social",
+      description: "二日で飽きた"
+    },
+    {
+      url: "https://steamcommunity.com/id/coiluck/",
+      title: "Steam",
+      description: "遊ぶ"
+    },
+    {
+      url: "https://misskey.io/@Asiraat/",
+      title: "Misskey",
+      description: "予備"
+    }
+  ]
+};
