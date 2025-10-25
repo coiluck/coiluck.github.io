83b69ecc2cfc5f4d2aa48528f8d846afb296ca87
coiluck <wvy.wingle@gmail.com>
2025-10-25 22:48:39 +0900

    Update style of code block
    Switch too my usual theme and override CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,7 +39,7 @@ export default defineConfig({
   integrations: [
     sitemap(),
     expressiveCode({
-      themes: ["github-dark-default", "github-light-default"],
+      themes: ["dark-plus", "github-light-default"],
     }),
   ]
 });
