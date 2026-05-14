d17b574661750617f78037c0af6fbc092facc975
coiluck <wvy.wingle@gmail.com>
2026-05-14 14:35:34 +0900

    New Style Starts

---
+++ src/env.d.ts
@@ -0,0 +1,23 @@
+declare module "@pagefind/default-ui" {
+  declare class PagefindUI {
+    constructor(opts: {
+      element?: string | HTMLElement,
+      bundlePath?: string,
+      pageSize?: number,
+      resetStyles?: boolean,
+      showImages?: boolean,
+      showSubResults?: boolean,
+      excerptLength?: number,
+      processResult?: any,
+      processTerm?: any,
+      showEmptyFilters?: boolean,
+      debounceTimeoutMs?: number,
+      mergeIndex?: any,
+      translations?: any,
+      autofocus?: boolean,
+      sort?: any,
+      filters?: any,
+      filterMode?: any,
+    })
+  }
+}
