0f076278aa4372237581d2d0f8eff4ef26bb7246
coiluck <wvy.wingle@gmail.com>
2026-06-04 12:10:19 +0900

    Fix: type error of plugin

--- astro.config.ts
+++ astro.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
         [remarkCollapse, {
           test: "目次|Contents",
           summary: "目次",
-        }]
+        }],
       ],
       rehypePlugins: [
         rehypeSlug,
--- src/plugins/rehype-link-blank.ts
+++ src/plugins/rehype-link-blank.ts
@@ -1,14 +1,15 @@
 // rehype-link-blank.ts
 import { visit } from 'unist-util-visit';
+import type { Root } from 'hast';
 
 export default function rehypeLinkBlank(targetClassNames: string[]) {
-  return (tree) => {
+  return (tree: Root) => {
     visit(tree, 'element', (node) => {
       if (node.tagName === 'a' && node.properties) {
         const classList = node.properties.class || node.properties.className;
         let classArray: string[] = [];
         if (Array.isArray(classList)) {
-          classArray = classList;
+          classArray = classList.filter((c): c is string => typeof c === 'string');
         } else if (typeof classList === 'string') {
           classArray = classList.split(' ');
         }
